e9836a6c00e7588a9da715b325162710c11e4e23
veerbal1 <veerbalsingh1@gmail.com>
2025-04-12 15:20:37 +0530

    Improved loading page UI

--- app/(pages)/loading.tsx
+++ app/(pages)/loading.tsx
@@ -1,8 +1,11 @@
+import { Loader } from 'lucide-react'
 import React from 'react'
 
 const Loading = () => {
     return (
-        <div>Loading...</div>
+        <div className='flex justify-center items-center w-full h-full'>
+            <Loader className='animate-spin' />
+        </div>
     )
 }
 
