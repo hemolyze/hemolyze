837a78f49509b8b9f2a9b5dfb323eb89660454be
veerbal1 <veerbalsingh1@gmail.com>
2025-04-16 19:13:08 +0530

    Added markdown in chatbot

---
+++ shared/components/ui/markdown.tsx
@@ -0,0 +1,20 @@
+'use client';
+
+import React from 'react';
+import { marked } from 'marked';
+
+interface MarkdownProps {
+  content: string;
+  className?: string;
+}
+
+const MarkdownComponent = ({ content, className = '' }: MarkdownProps) => {
+  return (
+    <div 
+      className={`prose dark:prose-invert max-w-none prose-sm ${className}`}
+      dangerouslySetInnerHTML={{ __html: marked(content) }}
+    />
+  );
+};
+
+export const Markdown = React.memo(MarkdownComponent); 
